cfab3675573c549730bedbc8f998c4faa0cc44f5
Abhishek <103208789+abhishekdotapp@users.noreply.github.com>
2025-03-10 15:29:42 +0000

    some basic  routing

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class onboarding extends Component {
+  render() {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text>onboarding</Text>
+      </View>
+    )
+  }
+}
